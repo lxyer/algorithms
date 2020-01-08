8e249f0a39eb87b795fca7ef3253c983f5e2554f
lxyer <489322130@qq.com>
2020-01-08 13:42:33 +0800

    Initial commit

---
+++ src/test/java/com/lxyer/algorithms/AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxyer.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
