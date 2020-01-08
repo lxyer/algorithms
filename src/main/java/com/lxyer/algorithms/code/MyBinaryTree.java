4166531e7561a0a6b4269fd218b31c8a11e04a09
lxyer <489322130@qq.com>
2020-01-08 17:43:51 +0800

    二叉树广度优先遍历

--- src/main/java/com/lxyer/algorithms/code/MyBinaryTree.java
+++ src/main/java/com/lxyer/algorithms/code/MyBinaryTree.java
@@ -1,7 +1,9 @@
 package com.lxyer.algorithms.code;
+import	java.util.Queue;
 
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class MyBinaryTree {
     public static TreeNode createBinaryTree(LinkedList<Integer> inputList) {
@@ -19,7 +21,8 @@ public class MyBinaryTree {
     }
 
     /**
-     * 二叉树前序遍历
+     * 二叉树前序遍历(递归)
+     *
      * @param node
      */
     public static void preOrder(TreeNode node) {
@@ -30,8 +33,10 @@ public class MyBinaryTree {
         preOrder(node.left);
         preOrder(node.right);
     }
+
     /**
-     * 二叉树中序遍历
+     * 二叉树中序遍历(递归)
+     *
      * @param node
      */
     public static void inOrder(TreeNode node) {
@@ -42,8 +47,10 @@ public class MyBinaryTree {
         System.out.println(node.data);
         inOrder(node.right);
     }
+
     /**
-     * 二叉树后序遍历
+     * 二叉树后序遍历(递归)
+     *
      * @param node
      */
     public static void postOrder(TreeNode node) {
@@ -54,6 +61,51 @@ public class MyBinaryTree {
         postOrder(node.right);
         System.out.println(node.data);
     }
+
+    /**
+     * 二叉树非递归前序遍历
+     *
+     * @param node
+     */
+    public static void preOrderTraveralWithStack(TreeNode node) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode treeNode = node;
+        while (treeNode != null || !stack.isEmpty()) {
+            //迭代访问节点左孩子,并入栈
+            if (treeNode != null) {
+                System.out.println(treeNode.data);
+                stack.push(treeNode);
+                treeNode = treeNode.left;
+            }
+            //如果左孩子没有了,则弹出栈顶节点,访问节点右孩子
+            if (!stack.empty()) {
+                treeNode = stack.pop();
+                treeNode = treeNode.right;
+            }
+        }
+    }
+
+    /**
+     * 二叉树层序遍历-广度优先
+     *
+     * @param node
+     */
+    public static void levelOrderTraversal(TreeNode node) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode> ();
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            TreeNode treeNode = queue.poll();
+            System.out.println(treeNode.data);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+    }
+
     public static class TreeNode {
         private Integer data;
         private TreeNode left;
